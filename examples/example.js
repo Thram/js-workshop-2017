830d1ede8407e3b6f0a2f036d7a565cb46f599d2
Thram <thramposo@gmail.com>
2017-04-19 17:10:47 +1200

    - Add example for Arrow Functions and Arrays

--- examples/example.js
+++ examples/example.js
@@ -4,8 +4,12 @@
 import variables from './variables';
 import dataTypes from './data_types';
 import assignments from './assignments';
+import arrowFunction from './arrow_function';
+import arrays from './arrays';
 
 variables();
 dataTypes();
 assignments();
+arrowFunction();
+arrays();
 
