a8ea1486332603a6e95ca864f427df44e93aaede
Thram <thramposo@gmail.com>
2017-04-19 19:46:52 +1200

    - Solution Exercise 2

--- src/index.js
+++ src/index.js
@@ -3,23 +3,42 @@
  * List all the items on the file:
  * - Filter the ones that have no stats
  * - Separate them by alignment
- * - Display them in 2 lists with the alignment as Title
+ * - Display them in different lists with the alignment as Title
  * - Display the total next to the title in the format "(number)"
  * - Render it into the DOM
  */
 
-import data from './data.json';
+import characters from './data.json';
 
-function createItem({ name }) {
-  return `<li>${name}</li>`;
-}
+const isEmpty = (obj = {}) => !Object.keys(obj).length;
+
+const createItem = ({ name }) => `<li>${name}</li>`;
+
+const createList = (title, items) => `
+    <h2>${title} (${items.length})</h2>
+    <ul>${items.map(createItem).join('')}</ul>
+`;
 
 const $root = document.getElementById('workshop');
-const $list = document.createElement('ul');
 
-$root.appendChild($list);
+const addToAlignment = (init = [], item) => init.concat(item);
 
-const [first] = data;
+const filterStatless = (data = []) => data.reduce((result, item) => {
+  const { stats } = item;
+  if (!isEmpty(stats)) result.push(item);
+  return result;
+}, []);
 
-$list.innerHTML += createItem(first);
+const byAlignment = (data = []) => data.reduce((result, item) => {
+  const alignment = item.biography.alignment;
+  result[alignment] = addToAlignment(result[alignment], item);
+  return result;
+}, {});
+
+const cleanList = filterStatless(characters);
+const alignments = byAlignment(cleanList);
+
+Object.keys(alignments).forEach((key) => {
+  $root.innerHTML += createList(key, alignments[key]);
+});
 
