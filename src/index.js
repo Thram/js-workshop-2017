31c10f25cec80c84f9b1205086117328649ca5e8
Thram <thramposo@gmail.com>
2017-04-19 22:36:40 +1200

    - Add base styles for the workshop (In progress)

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 /**
- * EXERCISE 2:
- * List all the items on the file:
- * - Filter the ones that have no stats
- * - Separate them by alignment
+ * EXERCISE 3:
+ * Use compose, pipe or curry to improve the code, also:
+ * - Display the list in columns and improve styles using `styles.js`
+ * - Make list behave like dropdowns
  * - Display them in different lists with the alignment as Title
  * - Display the total next to the title in the format "(number)"
  * - Render it into the DOM
@@ -15,19 +15,18 @@ const isEmpty = (obj = {}) => !Object.keys(obj).length;
 const createItem = ({ name }) => `<li>${name}</li>`;
 
 const createList = (title, items) => `
+    <div>
     <h2>${title} (${items.length})</h2>
     <ul>${items.map(createItem).join('')}</ul>
+    </div>
 `;
 
 const $root = document.getElementById('workshop');
 
 const addToAlignment = (init = [], item) => init.concat(item);
 
-const filterStatless = (data = []) => data.reduce((result, item) => {
-  const { stats } = item;
-  if (!isEmpty(stats)) result.push(item);
-  return result;
-}, []);
+const filterByStats = (data = []) => data.reduce((result, item) =>
+  (!isEmpty(item.stats) ? result.concat(item) : result), []);
 
 const byAlignment = (data = []) => data.reduce((result, item) => {
   const alignment = item.biography.alignment;
@@ -35,7 +34,7 @@ const byAlignment = (data = []) => data.reduce((result, item) => {
   return result;
 }, {});
 
-const cleanList = filterStatless(characters);
+const cleanList = filterByStats(characters);
 const alignments = byAlignment(cleanList);
 
 Object.keys(alignments).forEach((key) => {
---
+++ src/styles.js
@@ -0,0 +1,12 @@
+/**
+ * Created by thram on 19/04/17.
+ */
+export const COLORS = {
+  primary: '#050507',
+  secondary: '#F8D507',
+  terciary: '#32576A',
+  error: '#E42E45',
+  light: '#FFF3BE',
+};
+
+export const column = 'flex:1;';
