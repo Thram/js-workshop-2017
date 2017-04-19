7e6a5686803c45a5f3d5685e754e72d63039994e
Thram <thramposo@gmail.com>
2017-04-19 17:11:36 +1200

    - Solution for Exercise 1

--- src/index.js
+++ src/index.js
@@ -6,7 +6,18 @@
  * - Render it into the DOM
  */
 
+import data from './data.json';
+
+function createItem({ name }) {
+  return `<li>${name}</li>`;
+}
+
 const $root = document.getElementById('workshop');
 const $list = document.createElement('ul');
 
 $root.appendChild($list);
+
+const [first] = data;
+
+$list.innerHTML += createItem(first);
+
