f6882af0b648dae96b553424dca08feccba45134
Thram <thramposo@gmail.com>
2017-06-13 23:52:40 +1200

    Add data import

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@
  * - Create a function that receives an object and add a <li> to $list with the attribute `name`
  * - Render it into the DOM
  */
+import characters from './data.json';
 
 const $root = document.getElementById('workshop');
 const $list = document.createElement('ul');
